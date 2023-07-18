c37a057a26392cc7d2254102a83b10290481cff2
vinney-mo <vinneymotanya@gmail.com>
2023-07-18 15:16:10 +0300

    Updated the app.js file for db connection

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { mongoose } = require('mongoose');
 require('dotenv').config();
 
 const dataRoutes = require('./routes/data');
 
 const PORT = process.env.PORT || 3000;
 
-const connectionString = process.env.DATABASE_URL;
-mongoose.connect(connectionString);
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB:', err));
 
-const database = mongoose.connection;
-
-database.on('error', (error) => {
-    console.log(error);
-});
-
-database.on('connected', () => {
-    console.log('Database connected');
-});
 
 const app = express();
 app.use(express.json());
