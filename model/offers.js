6954c9a2c8a2949f0658954e95d2a0bbaf927e64
vinney-mo <vinneymotanya@gmail.com>
2023-07-20 14:10:24 +0300

    Update fetch criteria

--- model/offers.js
+++ model/offers.js
@@ -5,7 +5,7 @@ async function fetchOffersInRange(userAmount, type) {
     try {
         const resourceType = (type === "1") ? "data" : "voice";
         return await Offers.find({
-            amount: {$gte: userAmount - 10, $lte: userAmount + 10},
+            amount: {$gte: parseFloat(userAmount) - 10, $lte: parseFloat(userAmount) + 10},
             type: resourceType
         });
     } catch (err) {
